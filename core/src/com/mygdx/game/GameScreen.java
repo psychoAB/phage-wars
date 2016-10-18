9f464d1e9b41fdce29d9d6808041404eb73908f4
psychoAB <dome.2499@hotmail.com>
2016-10-18 18:16:32 +0700

    change screen to GameScreen class

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -3,5 +3,13 @@ package com.mygdx.game;
 import com.badlogic.gdx.ScreenAdapter;
 
 public class GameScreen extends ScreenAdapter {
+    private PhageWarsGame phageWarsGame;
+    
+    public GameScreen(PhageWarsGame phageWarsGame) {
+        this.phageWarsGame = phageWarsGame;
+    }
 
+    @Override public void render(float delta) {
+        System.out.println(delta);
+    }
 }
--- core/src/com/mygdx/game/PhageWarsGame.java
+++ core/src/com/mygdx/game/PhageWarsGame.java
@@ -11,6 +11,7 @@ public class PhageWarsGame extends Game {
 
     @Override public void create () {
         batch = new SpriteBatch();
+        setScreen(new GameScreen(this));
     }
 
     @Override public void render () {

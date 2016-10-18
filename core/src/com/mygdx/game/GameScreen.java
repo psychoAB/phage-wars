335cbd464bd21e1bc7c6d6fd807eddea37b24487
psychoAB <dome.2499@hotmail.com>
2016-10-18 23:38:24 +0700

    render backgrond

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,15 +1,25 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
 
 public class GameScreen extends ScreenAdapter {
     private PhageWarsGame phageWarsGame;
     
+    private Texture background;
+    
     public GameScreen(PhageWarsGame phageWarsGame) {
         this.phageWarsGame = phageWarsGame;
+
+        background = new Texture("background.png");
     }
 
     @Override public void render(float delta) {
-        System.out.println(delta);
+        SpriteBatch batch = phageWarsGame.batch;
+
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.end();
     }
 }

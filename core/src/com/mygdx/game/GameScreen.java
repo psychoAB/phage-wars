7f04f593edd169b7099e3e44df1d003092a68681
psychoAB <dome.2499@hotmail.com>
2016-11-04 04:34:24 +0700

    refactor GameScreen render to World render

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -4,45 +4,28 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class GameScreen extends ScreenAdapter {
     private PhageWarsGame phageWarsGame;
     
     private World world;
 
-    private Cell cell;
-    
-    private Texture background;
-    private Texture cellImage;
-
-    private BitmapFont font;
-    
     public GameScreen(PhageWarsGame phageWarsGame) {
         this.phageWarsGame = phageWarsGame;
 
-        world = new World();
-        cell = new Cell(100, 100, world);
-        background = new Texture("background.png");
-        cellImage = new Texture("cell.png");
-        font = new BitmapFont();
+        world = new World(phageWarsGame);
     }
 
     @Override public void render(float delta) {
         SpriteBatch batch = phageWarsGame.batch;
-
-        Vector2 cellPosition = cell.getPosition();
         
         batch.begin();
         
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.draw(background, 0, 0);
-        batch.draw(cellImage, cellPosition.x, cellPosition.y);
-        font.draw(batch, "" + cell.getVirusNumber(), cellPosition.x + cellImage.getWidth() / 2, cellPosition.y + cellImage.getHeight() / 2);
         batch.end();
+
+        world.render(delta);
     }
 }
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,10 +1,49 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+import java.util.LinkedList;
+
 public class World {
+    
+    private PhageWarsGame phageWarsGame;
 
     public WorldTimer worldTimer;
 
-    public World() {
+    private LinkedList<Cell> cells;
+    
+    private Texture background;
+    private Texture cellImage;
+
+    private BitmapFont font;
+
+    public World(PhageWarsGame phageWarsGame) {
+        this.phageWarsGame = phageWarsGame;
+
         worldTimer = new WorldTimer();
+        cells = new LinkedList<Cell>();
+        background = new Texture("background.png");
+        cellImage = new Texture("cell.png");
+        font = new BitmapFont();
+
+        cells.add(new Cell(100, 100, this));
+    }
+
+    public void render(float delta) {
+        SpriteBatch batch = phageWarsGame.batch;
+
+        batch.begin();
+
+        batch.draw(background, 0, 0);
+
+        for(Cell cell : cells) {
+            Vector2 cellPosition = cell.getPosition();
+            batch.draw(cellImage, cellPosition.x, cellPosition.y);
+            font.draw(batch, "" + cell.getVirusNumber(), cellPosition.x + Cell.IMAGE_SIZE / 2, cellPosition.y + Cell.IMAGE_SIZE / 2);
+        }
+
+        batch.end();
     }
 }

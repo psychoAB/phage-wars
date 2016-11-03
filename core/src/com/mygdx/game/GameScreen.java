de3e444932a3f04e3c4dad20cd3bffd018a3977a
psychoAB <dome.2499@hotmail.com>
2016-11-04 00:40:49 +0700

    GameScreen clear every frame

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 
@@ -19,6 +21,10 @@ public class GameScreen extends ScreenAdapter {
         SpriteBatch batch = phageWarsGame.batch;
 
         batch.begin();
+        
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.draw(background, 0, 0);
         batch.end();
     }

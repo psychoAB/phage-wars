effad966ea6d1714dfea85e3956cc06859e7d86a
psychoAB <dome.2499@hotmail.com>
2016-11-04 01:54:10 +0700

    Cell have regeneration rate

--- core/src/com/mygdx/game/Cell.java
+++ core/src/com/mygdx/game/Cell.java
@@ -5,13 +5,14 @@ import com.badlogic.gdx.math.Vector2;
 public class Cell {
     
     private Vector2 position;
-    
     private int virusNumber;
+    private int regenerationRate;
 
     public Cell(int x, int y) {
         position = new Vector2(x, y);
 
         virusNumber = 0;
+        regenerationRate = 0;
     }
 
     public Vector2 getPosition() {

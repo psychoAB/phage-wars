e5d3b19e94eafa0f6a86786fb6393855a79f9b2b
psychoAB <dome.2499@hotmail.com>
2016-11-03 23:36:26 +0700

    cell have postion

--- core/src/com/mygdx/game/Cell.java
+++ core/src/com/mygdx/game/Cell.java
@@ -1,4 +1,16 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Cell {
+    
+    private Vector2 position;
+
+    public Cell(int x, int y) {
+        position = new Vector2(x, y);
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
 }

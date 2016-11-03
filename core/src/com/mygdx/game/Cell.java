72479858ecc4a7575e6856409862fa878b54336f
psychoAB <dome.2499@hotmail.com>
2016-11-04 00:09:48 +0700

    cell have virusNUmber

--- core/src/com/mygdx/game/Cell.java
+++ core/src/com/mygdx/game/Cell.java
@@ -5,12 +5,20 @@ import com.badlogic.gdx.math.Vector2;
 public class Cell {
     
     private Vector2 position;
+    
+    private int virusNumber;
 
     public Cell(int x, int y) {
         position = new Vector2(x, y);
+
+        virusNumber = 0;
     }
 
     public Vector2 getPosition() {
         return position;
+    }
+
+    public int getVirusNumber() {
+        return virusNumber;
     }
 }

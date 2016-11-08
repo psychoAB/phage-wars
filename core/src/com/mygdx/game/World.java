712aad1388de3de74d2f039251f0d3dfbe0c1896
psychoAB <dome.2499@hotmail.com>
2016-11-09 01:30:14 +0700

    create init cell by array of int

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -4,6 +4,16 @@ import java.util.LinkedList;
 
 public class World {
     
+    private static final int [][] initCellPosition = new int [][] {
+        {100, PhageWarsGame.HEIGHT / 2},
+        {PhageWarsGame.WIDTH - 100, PhageWarsGame.HEIGHT / 2},
+        {PhageWarsGame.WIDTH / 2, PhageWarsGame.HEIGHT / 2},
+        {PhageWarsGame.WIDTH / 4 + 50, PhageWarsGame.HEIGHT / 4},
+        {PhageWarsGame.WIDTH / 4 + 50, PhageWarsGame.HEIGHT * 3 / 4},
+        {PhageWarsGame.WIDTH * 3 / 4 - 50, PhageWarsGame.HEIGHT / 4},
+        {PhageWarsGame.WIDTH * 3 / 4 - 50, PhageWarsGame.HEIGHT * 3 / 4}
+    };
+
     public WorldTimer worldTimer;
 
     private LinkedList<Cell> cells;
@@ -12,7 +22,9 @@ public class World {
         worldTimer = new WorldTimer();
         cells = new LinkedList<Cell>();
 
-        cells.add(new Cell(0, 0, this));
+        for(int [] cellPosition : initCellPosition) {
+            cells.add(new Cell(cellPosition[0], cellPosition[1], this));
+        }
     }
 
     public LinkedList<Cell> getCells() {
